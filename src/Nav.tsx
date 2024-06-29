3ded5e50683f63d9987088c40eb8d19d2b25b0f1
killmenowya <handthad@gmail.com>
2024-06-29 21:24:25 +0800

    modified:   src/App.tsx 	modified:   src/Nav.tsx 	modified:   src/components/dashboard/Sidebar.tsx 	renamed:    src/components/dashboard/pages/MainCalendar.tsx -> src/components/dashboard/comp/MainCalendar.tsx 	modified:   src/components/dashboard/comp/calendar/UserCalendar.tsx 	new file:   src/components/dashboard/pages/Calendar.tsx

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -2,11 +2,10 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./components/dashboard/pages/Dashboard";
 import ForgetPassword from "./components/ForgetPassword";
 import LogInPage from "./components/LogInPage";
-import Calendar from "./components/dashboard/comp/calendar/UserCalendar";
 import CoursePlanning from "./components/dashboard/pages/CoursePlanning";
-import Modules from "./components/dashboard/pages/Modules";
 import UserSettings from "./components/dashboard/pages/UserSetting";
-import MainCalendar from "./components/dashboard/pages/MainCalendar";
+import MainCalendar from "./components/dashboard/comp/MainCalendar";
+import Calendar from "./components/dashboard/pages/Calendar";
 
 // this is where we put all our routes. DO NOT SHOVE ALL INTO ONE FILE OOP!
 // ================================
@@ -16,9 +15,8 @@ const Nav = () => {
       <Route path={"/"} element={<LogInPage />} />
       <Route path={"/forget"} element={<ForgetPassword />} />
       <Route path="/main" element={<Dashboard />} />
-      <Route index element={<MainCalendar />} />
+      <Route path="/main/calendar" element={<Calendar />} />
       <Route path="/main/courseplanning" element={<CoursePlanning />} />
-      <Route path="/main/modules" element={<Modules />} />
       <Route path="/main/usersettings" element={<UserSettings />} />
     </Routes>
   );
---
+++ src/components/dashboard/pages/Calendar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Box, Divider, Grid, Stack } from "@mui/material";
+import Sidebar from "../Sidebar";
+import Topbar from "../Topbar";
+import Graph from "../comp/Graph";
+import MainCalendar from "../comp/MainCalendar";
+
+const Calendar = () => {
+    return (
+        <Box sx={{ height: '100vh', display: 'flex', flexDirection: 'column' , width: '100vw'}}>
+            
+              <Topbar pageTitle="Calendar"/>
+              
+              <Box sx={{ flex: 1, display: 'flex'}}>
+            <Stack 
+              direction={'row'}  
+              spacing={0}
+              justifyContent={'space-between'}
+              sx={{ width: '100%', height: '100%' }}
+              >
+              <Sidebar/>
+              <Divider orientation="vertical" flexItem />
+              <MainCalendar/>
+              
+            </Stack>
+            </Box>  
+        </Box>
+      );
+}
+
+export default Calendar
